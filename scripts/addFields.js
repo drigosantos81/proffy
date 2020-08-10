3170bffd4e4b122eb296c7db8363cb8f32bd1e2f
Rodrigo Emanuel <drigosantos@gmail.com>
2020-08-09 22:16:58 -0300

    Adicionado função de clonar elemento.

--- scripts/addFields.js
+++ scripts/addFields.js
@@ -1,5 +1,12 @@
 document.querySelector("#add-time").addEventListener('click', cloneField);
 
 function cloneField() {
-    console.log("Cheguei aqui");
+    const newFieldsContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+    const fields = newFieldsContainer.querySelectorAll('input');
+    fields.forEach(function(field) {
+        field.value = "";
+    });
+    
+    document.querySelector('#schedule-items').appendChild(newFieldsContainer);
 }

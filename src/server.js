9dd013c62334d2a2c5658075bf9bbecfae96b248
Rodrigo Emanuel <drigosantos@gmail.com>
2020-08-09 23:32:51 -0300

    Adicionado dados dinamicos dentro do server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,54 @@
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "8995412445", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220] 
+    },
+    { 
+        name: "Rodrigo Emanuel", 
+        avatar: "https://avatars0.githubusercontent.com/u/52610125?s=460&u=48f0753e5d5799e1faabe89fa277cc57f5eb40fb&v=4", 
+        whatsapp: "8995412445", 
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "35", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    }
+]
+
 const express = require('express');
 const server = express();
+const nunjucks = require('nunjucks');
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html");
-})
-.get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html");
-})
-.get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html");
-})
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+});
+
+function pageLanding(req, res) {
+    return res.render("index.html");
+}
+
+function pageStudy(req, res) {
+    return res.render("study.html", { proffys });
+}
+
+function pageGiveClasses(req, res) {
+    return res.render("give-classes.html");
+}
+
+server
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(5500, () => {
     console.log('Servidor rodando');
 });
